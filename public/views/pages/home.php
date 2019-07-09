22eab71a8e5c7fc33372f993d30e8aa1ce928d53
Hotille Valère <hc.valere@hotmail.fr>
2019-07-09 16:54:37 +0200

    final

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -2,12 +2,6 @@
 
 <h1><center>UTC_VALERE Concessionnaire</center></h1><br><br>
 
-<!-- <ul>
-    <li><a href="<?= url('conducteurs') ?>">Conducteur</li>
-    <li><a href="<?= url('associations') ?>">Association_vehicule_conducteur</li>
-    <li><a href="<?= url('vehicules') ?>">Vehicule</li>
-</ul> -->
-
 
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
